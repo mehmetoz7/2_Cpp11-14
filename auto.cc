11a91c218342954bd0a6aa0ba49c5f501f62d059
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-02-20 20:19:54 +0300

    auto tutorial

---
+++ auto.cc
@@ -0,0 +1,46 @@
+#include<iostream>
+#include <vector> 
+#include <map>
+  
+using namespace std;
+  
+auto meaningOfLife()    
+{
+   return 42; 
+} 
+
+template<typename T, typename U>
+auto add (T t, U u)
+{
+   return t+u;
+}
+ 
+int main(){
+    cout << meaningOfLife() << endl;
+    cout << add(3, 4.5) << endl;
+    cout << add(string("ali"), string(" oz")) << endl;    
+
+    vector<double> values;
+    typedef decltype(values.begin()) dvi; //double vector iterator    
+    typedef decltype(42) myint;
+    typedef decltype(add(3, 4.5)) qtype;
+    
+    vector< int > v = { 1, 2, 3, 4, 5, 6 };    
+    //vector< int >::const_iterator iter;     
+    for (auto iter = v.begin(); iter!= v.end(); ++iter )
+       cout << *iter << ' ';            
+    cout << endl;
+   
+    auto no = 45;  // int
+    auto name = "Marco Van Basten"; // std::string
+    auto wage = 12.34;  // double
+
+    cout << no << " " << name << " " << wage << endl;
+
+    auto d = {1,2,3}; //list
+    map<string, vector<double>> ticks;
+    //map<string, vector<double>>::iterator it = ticks.begin();
+    auto it = ticks.begin();    
+    
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,15 @@
+all: my_exec
+
+my_exec: auto.o
+	g++ auto.o -o auto
+
+auto.o: auto.cc
+	g++ -std=c++14 -c auto.cc
+	
+.PHONY: clean help
+clean:
+	rm auto *.o
+
+help:
+	@echo "make"
+	@echo "make clean"
