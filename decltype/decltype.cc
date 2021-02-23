2d6252a4756e302a36922072b7819bfebf490a08
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-02-23 19:23:25 +0300

    decltype: explanatory comments

--- decltype/decltype.cc
+++ decltype/decltype.cc
@@ -8,12 +8,14 @@ auto add (T a, T b){
 }
 
 int main(){
+    //type of x is return type of the function
     decltype (add(5, 6)) x;
     cout << typeid(x).name() << endl;
     
     decltype (add(5.9, 6.8)) y;
     cout << typeid(y).name() << endl;
-    
+
+    //type of z is return type of x    
     decltype(x) z = 5;
     cout << typeid(z).name() << endl;
    
