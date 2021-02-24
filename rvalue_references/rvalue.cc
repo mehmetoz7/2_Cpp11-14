0acb5dbb9e244e77daeff587e6733eed3ff614c6
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-02-24 12:50:58 +0300

    move constructor added

--- rvalue_references/rvalue.cc
+++ rvalue_references/rvalue.cc
@@ -1,15 +1,61 @@
-/*enables to distinguish an lvalue from an rvalue
-
-*/
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-void f(int& i){cout << "lvalue" << endl;}
+//enables to distinguish an lvalue from an rvalue
+void f(int& i) {cout << "lvalue" << endl;}
 void f(int&& i){cout << "rvalue" << endl;}
 
 //return of the function is an rvalue
 int get(){int i=10; return i;}
+
+class Test{
+private:
+    string *ptr;
+public:
+    Test()=default;
+
+    //constructor
+    Test(const string& s):ptr(new string(s)){
+       cout << "constructor" << endl;       
+    }
+    
+    //destructor
+    ~Test(){delete ptr;}
+
+    //move constructor
+    Test(Test&& x) : ptr(x.ptr) {
+       cout << "move constructor" << endl;       
+       x.ptr = nullptr;
+    } 
+    
+    //copy constructor
+    Test (const Test& x):ptr(new string(*x.ptr)){
+       cout << "copy constructor" << endl;
+    }
+    
+    //copy assignment
+    Test& operator=(const Test& x){
+       cout << "copy assignment" << endl;    
+       *ptr = *x.ptr;
+       return *this;
+    }
+     
+    //move assignment
+    Test& operator=(Test&& x){
+       cout << "move assignment" << endl;
+       delete ptr;
+       ptr = x.ptr;
+       x.ptr = nullptr;
+       return *this;
+    }    
+};
+
+Test f(Test a){
+   return a;
+}
 
 int main(){
     int&& rvalue_ref = 50;
@@ -17,16 +63,16 @@ int main(){
     int x = 80;    
     int& lvalue_ref = x;
     
-    //in C++0x lvalue reference can bind to a temporary objects 
-    //if it is const 
-    const int& val = get();        
-    
+    //in C++0x if lvalue reference is const, 
+    //it can bind to a temporary objects 
+    const int& val = get(); 
+           
     //but otherwise can only be assigned to a normal reference
     //so these are not compilable
     //int& lvalue_ref = 40;   
-    //int& lvalue_ref = get();        
-
-    //these both are compilable in C++11
+    //int& lvalue_ref = get();      
+    
+    //in C++11 using rvalue reference these lines are compilable
     const int&& val2 = get();
     int&& val3 = get();
     //---------------------------------
@@ -45,13 +91,21 @@ int main(){
     
     //tha main usage of rvalue references are to create 
     //move constructor and move assignment
-    
-    
-    
-    
-    
-
+    cout << "----------------" << endl;
+    Test t1{"111"};    
+    Test t2{"222"};
+    Test t3 = t1;               //copy constructor
+    t3 = t2;                    //copy assignment
+    cout << "----------------" << endl;    
+/*    
+    vector<Test> v;   
+    v.push_back(Test{"aaa"});   //move constructor 
+    v.push_back(Test{"bbb"});   //move constructor    
+*/
+    t1 = f(Test());             //move assignment     
+    Test t5 = f(Test());        //move constructor
     return 0;
 }
+
 
 
