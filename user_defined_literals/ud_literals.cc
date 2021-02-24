d7c177b43bcc0e30ea94dd9473701540b89bc62d
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-02-24 20:24:47 +0300

    user defined literals sample

--- user_defined_literals/ud_literals.cc
+++ user_defined_literals/ud_literals.cc
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <iomanip>
   
 using namespace std;
 
+long double operator"" _kg(long double x){return x * 1000;}
+long double operator""  _g(long double x){return x;}
+long double operator"" _mg(long double x){return x / 1000;}
+
 int main(){
-    
+    long double w = 10.0_kg;
+    cout << 10.0_kg / 1.0_g << endl;
     return 0;
 }
 
