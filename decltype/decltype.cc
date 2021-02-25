90f7ee14bc12a25095f928ef69b7daba6bd0a1de
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-02-25 20:52:45 +0300

    declval added

--- decltype/decltype.cc
+++ decltype/decltype.cc
@@ -1,11 +1,19 @@
 #include <iostream>
-  
+#include <utility>
+#include <vector>  
+
 using namespace std;
 
 template <typename T>
 auto add (T a, T b){
    return a + b;
 }
+
+class B {    // class with specific constructor
+  int val_=5;
+public:
+  int value() {return val_;}
+};
 
 int main(){
     //type of x is return type of the function
@@ -18,6 +26,17 @@ int main(){
     //type of z is return type of x    
     decltype(x) z = 5;
     cout << typeid(z).name() << endl;
-   
+    
+    //if there is no existing lvalue, declval is used
+    decltype(declval<B>().value()) i;
+    cout << typeid(i).name() << endl;
+    
+    decltype(B(declval<B>())) b; //class B instance
+    cout << b.value() << endl;
+
     return 0;
 }
+
+
+
+
