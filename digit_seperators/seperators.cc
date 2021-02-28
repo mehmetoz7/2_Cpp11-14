389d83c4452307e5c70bec79340a0a12de8fe2f0
mehmetrizaoz <mehmetrizaoz@gmail.com>
2021-02-28 16:59:46 +0300

    digit seperators sample code

---
+++ digit_seperators/seperators.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+  
+using namespace std;
+
+int main(){   
+   
+   auto million = 1'000'000;
+   auto pi = 3.14159'26535'89793;
+   cout << million << " " << pi << endl;
+       
+   return 0;
+}
+
+
+
